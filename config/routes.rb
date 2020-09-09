dbaaef5d3e7d5dc362f339c96985844d7591f06e
vagrant <vagrant@localhost.localdomain>
2020-09-09 07:33:49 +0000

    add follow

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,20 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "home#index"
   get 'home/about'
+
   resources :users, only: [:index, :show, :edit, :update]
+
   resources :books do
     resource :favorites, only: [:create, :destroy]
   	resources :book_comments, only: [:create, :destroy]
   end
+
+  resources :relationships, only: [:create, :destroy]
+
+  resources :users do
+    member do
+      get :followings, :followers
+    end
+  end
+
 end
